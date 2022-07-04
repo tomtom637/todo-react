826cedb916a1c86c0e6ad4e5a0dc51b1d0d5c887
Thomas Lacombe <tomtom637@gmail.com>
2022-07-04 02:09:41 +0200

    misc

--- src/store.js
+++ src/store.js
@@ -4,7 +4,7 @@ import { atomWithStorage } from 'jotai/utils';
 // SET THE INITIAL VALUE OF OUR LOCAL STORAGE LISTS OF TODOS
 export const todosAtom = atomWithStorage('todos', []);
 
-export const stopDisplayingModalAtom = atomWithStorage('displayModal', false);
+export const stopDisplayingModalAtom = atomWithStorage('stopDisplayModal', false);
 
 // GLOBAL STATES
 export const displayModalAtom = atom(false);
